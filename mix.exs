6b7d565c901c68085e132b29791fbdb7b9b9dcbd
Fernando Lopez <fer.ragnar19@gmail.com>
2017-12-27 11:58:42 -0300

    actualizamos versiones
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- mix.exs
+++ mix.exs
@@ -20,7 +20,8 @@ defmodule LoginEjemplo.Mixfile do
   def application do
     [
       mod: {LoginEjemplo, []},
-      extra_applications: [:logger, :coherence]
+      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
+      :phoenix_ecto, :postgrex, :coherence]
     ]
   end
 
@@ -33,11 +34,11 @@ defmodule LoginEjemplo.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0-rc"},
-      {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},    
+      {:phoenix_ecto, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.10"},
+      {:phoenix_html, "~> 2.6"},
       {:coherence, "~> 0.3"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
