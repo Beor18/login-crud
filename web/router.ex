50865b1a63b496a33709e27b479ca76bf0d16de3
Fernando Lopez <fer.ragnar19@gmail.com>
2017-12-27 09:16:57 -0300

    agregado canalizacion protegida
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- web/router.ex
+++ web/router.ex
@@ -1,5 +1,8 @@
 defmodule LoginEjemplo.Router do
   use LoginEjemplo.Web, :router
+  
+  #Enrutador Coherence: Agregar Coherence.Router nos dará acceso a las rutas que Coherence creó para nosotros.
+  use Coherence.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -7,6 +10,17 @@ defmodule LoginEjemplo.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session #agregado
+  end
+
+  #Agregado 
+  pipeline :protected do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session, protected: true
   end
 
   pipeline :api do
